43cf23e5f7a23e215926f0552ca6f5b13793774d
alessiavozzo <a.vozzo95@gmail.com>
2024-03-20 14:31:14 +0100

    add hello world

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      hello: "Hello World"
+    }
+  }
+}
+</script>
+
+
+<template>
+  <h1>{{ hello }}</h1>
+</template>
+
+
+
+<style>
+h1 {
+  text-align: center;
+  font-size: 10rem;
+}
+</style>
